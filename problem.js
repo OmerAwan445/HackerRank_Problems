edc664c0c694e2929d64234a9ead60517481192a
Muhammad Umer <omerawan445@gmail.com>
2023-05-17 00:03:22 +0500

    Completed

--- problem.js
+++ problem.js
@@ -1,22 +1,82 @@
-// =============
-function hourglassSum(arr) {
-    let currSum = 0;
-    let maxSum;
-    //loop running rows
-    for (let i = 0; i < arr.length - 2; i++) {
-    //loop running columns
-        for (let j = 0; j < arr[i].length - 2; j++) {
-            currSum=arr[i][j]+arr[i][j+1]+arr[i][j+2];
-            currSum+=arr[i+1][j+1];
-            currSum+=arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
-            if(i==0 && j==0) maxSum = currSum;
-        if(maxSum < currSum) maxSum = currSum;
-        currSum = 0;
-    }
-}
-    return maxSum;
+// function processData(input) {
+//     //Enter your code here
+//     var input = input.split("\n");
+//     var firstLine = input[0].split(" ");
+//     var rotator = firstLine[1];
+//     var arr = input[1].split(" ");
+
+//     var simplifiedRotator = rotator % arr.length;
+
+//     console.log(arr.slice(simplifiedRotator, arr.length).concat(arr.slice(0, simplifiedRotator)).join(" "))
+// }
+
+// process.stdin.resume();
+// process.stdin.setEncoding("ascii");
+// _input = "";
+// process.stdin.on("data", function (input) {
+//     _input += input;
+// });
+
+// process.stdin.on("end", function () {
+//    processData(_input);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//============== Left Rotation ==================
+function rotateLeft(d, arr) {
+    let rotatedArr = [...arr];
+    const toPushArr = rotatedArr.splice(0,(d%arr.length));
+    rotatedArr=[...rotatedArr,...toPushArr];
+    return rotatedArr;
 }
 
-const twoDArr = [[-1, -1, 0, -9, -2, -2], [-2, -1, -6, -8, -2, -5], [-1, -1, -1, -2, -3, -4], [-1, -9, -2 ,-4 ,-4 ,-5], [-7, -3, -3 ,-2, -9, -9], [-1, -3, -1, -2, -4 ,-5]];
-const res = hourglassSum(twoDArr);
-console.log(res)
+const res = rotateLeft(5,[1,2,3]);
+console.log(res);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//============== Left Rotation ==================
+/* function rotateLeft(d, arr) {
+    let rotatedArr = [...arr];
+    const toPushArr = rotatedArr.splice(0,d);
+    rotatedArr=[...rotatedArr,...toPushArr];
+    return rotatedArr;
+}
+
+const res = rotateLeft(2,[1,2,3,4,5,2,4,5]);
+console.log(res); */
