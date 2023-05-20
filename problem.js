8b1d06ee17489b1fbbcbafdc704f971be04e41a5
Muhammad Umer <omerawan445@gmail.com>
2023-05-20 20:49:55 +0500

    Completed

--- problem.js
+++ problem.js
@@ -1,22 +1,30 @@
-// =============
-function hourglassSum(arr) {
-    let currSum = 0;
-    let maxSum;
-    //loop running rows
-    for (let i = 0; i < arr.length - 2; i++) {
-    //loop running columns
-        for (let j = 0; j < arr[i].length - 2; j++) {
-            currSum=arr[i][j]+arr[i][j+1]+arr[i][j+2];
-            currSum+=arr[i+1][j+1];
-            currSum+=arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
-            if(i==0 && j==0) maxSum = currSum;
-        if(maxSum < currSum) maxSum = currSum;
-        currSum = 0;
+
+
+function findAllIndex(str,queries){
+    return queries.reduce((indexes,el,i)=>{
+     if(el===str){
+     indexes.push(i);
     }
+    return indexes;
+    },[])
 }
-    return maxSum;
+function matchingStrings(stringList, queries) {
+    // Write your code here
+    const freqArr=[];
+for (let i = 0; i < queries.length; i++) {
+    freqArr[i]=0;
+}
+    stringList.forEach(str => {
+    const foundIndexes= findAllIndex(str,queries);
+    foundIndexes.map(index =>{
+        freqArr[index]++;
+    })
+});
+return freqArr;
 }
 
-const twoDArr = [[-1, -1, 0, -9, -2, -2], [-2, -1, -6, -8, -2, -5], [-1, -1, -1, -2, -3, -4], [-1, -9, -2 ,-4 ,-4 ,-5], [-7, -3, -3 ,-2, -9, -9], [-1, -3, -1, -2, -4 ,-5]];
-const res = hourglassSum(twoDArr);
-console.log(res)
+
+const res = matchingStrings(["lekgdisnsbfdzpqlkg","eagemhdygyv","jwvwwnrhuai","urcadmrwlqe","mpgqsvxrijpombyv","mpgqsvxrijpombyv","urcadmrwlqe","mpgqsvxrijpombyv","eagemhdygyv","eagemhdygyv","jwvwwnrhuai","urcadmrwlqe","jwvwwnrhuai","kvugevicpsdf","kvugevicpsdf","mpgqsvxrijpombyv","urcadmrwlqe","mpgqsvxrijpombyv","exdafbnobg","qhootohpnfvbl","suffrbmqgnln","exdafbnobg","exdafbnobg","eagemhdygyv","mpgqsvxrijpombyv","urcadmrwlq  e","jwvwwnrhuai","lekgdisnsbfdzpqlkg","mpgqsvxrijpombyv","lekgdisnsbfdzpqlkg","jwvwwnrhuai","exdafbnobg","mpgqsvxrijpombyv","kvugevicpsdf","qhootohpnfvbl","urcadmrwlqe","kvugevicpsdf","mpgqsvxrijpombyv","lekgdisnsbfdzpqlkg","mpgqsvxrijpombyv","kvugevicpsdf","qhootohpnfvbl","lxyqetmgdbmh","urcadmrwlqe","urcadmrwlqe","kvugevicpsdf","lxyqetmgdbmh","urcadmrwlqe","lxyqetmgdbmh","jwvwwnrhuai","qhootohpnfvbl","qhootohpnfvbl","jwvwwnrhuai","lekgdisnsbfdzpqlkg","kvugevicpsdf","mpgqsvxrijpombyv","exdafbnobg","kvugevicpsdf","lekgdisnsbfdzpqlkg","qhootohpnfvbl","exdafbnobg","qhootohpnfvbl","exdafbnobg","mpgqsvxrijpombyv","suffrbmqgnln","mpgqsvxrijpombyv","qhootohpnfvbl","jwvwwnrhuai","mpgqsvxrijpombyv","qhootohpnfvbl","lekgdisnsbfdzpqlkg","eagemhdygyv","jwvwwnrhuai","kvugevicpsdf","eagemhdygyv","eagemhdygyv","lxyqetmgdbmh","qhootohpnfvbl","lxyqetmgdbmh","exdafbnobg","qhootohpnfvbl","qhootohpnfvbl","exdafbnobg","suffrbmqgnln","mpgqsvxrijpombyv","urcadmrwlqe","eagemhdygyv","lxyqetmgdbmh","urcadmrwlqe","suffrbmqgnln","qhootohpnfvbl","kvugevicpsdf","lekgdisnsbfdzpqlkg","lxyqetmgdbmh","mpgqsvxrijpombyv","jwvwwnrhuai","lxyqetmgdbmh","lxyqetmgdbmh","lekgdisnsbfdzpqlkg","qhootohpnfvbl"]
+,
+["exdafbnobg","eagemhdygyv","mpgqsvxrijpombyv","kvugevicpsdf","lekgdisnsbfdzpqlkg","kvugevicpsdf","exdafbnobg","qhootohpnfvbl","eagemhdygyv","kvugevicpsdf","suffrbmqgnln","jwvwwnrhuai","lekgdisnsbfdzpqlkg","lekgdisnsbfdzpqlkg","mpgqsvxrijpombyv","jwvwwnrhuai","kvugevicpsdf","lekgdisnsbfdzpqlkg","exdafbnobg","suffrbmqgnln","qhootohpnfvbl","eagemhdygyv","exdafbnobg","suffrbmqgnln","jwvwwnrhuai","qhootohpnfvbl","eagemhdygyv","exdafbnobg","exdafbnobg","jwvwwnrhuai","qhootohpnfvbl","lxyqetmgdbmh","qhootohpnfvbl","suffrbmqgnln","lxyqetmgdbmh","qhootohpnfvbl","eagemhdygyv","jwvwwnrhuai","eagemhdygyv","qhootohpnfvbl","mpgqsvxrijpombyv","qhootohpnfvbl","jwvwwnrhuai","exdafbnobg","eagemhdygyv","eagemhdygyv","kvugevicpsdf","kvugevicpsdf","jwvwwnrhuai","urcadmrwlqe","lxyqetmgdbmh","qhootohpnfvbl","exdafbnobg","exdafbnobg","eagemhdygyv","qhootohpnfvbl","exdafbnobg","exdafbnobg","lekgdisnsbfdzpqlkg","jwvwwnrhuai","eagemhdygyv","urcadmrwlqe","kvugevicpsdf","lekgdisnsbfdzpqlkg","jwvwwnrhuai","eagemhdygyv","lekgdisnsbfdzpqlkg","exdafbnobg","kvugevicpsdf","jwvwwnrhuai","exdafbnobg","lxyqetmgdbmh","exdafbnobg","lxyqetmgdbmh","jwvwwnrhuai","mpgqsvxrijpombyv","eagemhdygyv","urcadmrwlqe","kvugevicpsdf","qhootohpnfvbl","jwvwwnrhuai","eagemhdygyv","urcadmrwlqe","urcadmrwlqe","exdafbnobg","qhootohpnfvbl","exdafbnobg","eagemhdygyv","exdafbnobg","jwvwwnrhuai","eagemhdygyv","jwvwwnrhuai","mpgqsvxrijpombyv","urcadmrwlqe","urcadmrwlqe","eagemhdygyv","eagemhdygyv","jwvwwnrhuai","suffrbmqgnln","eagemhdygyv",]);
+console.log(res);
