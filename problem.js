4eff17acf94c0fbe49ca185e4b4a68ba26f8bedf
Muhammad Umer <omerawan445@gmail.com>
2023-05-27 03:29:18 +0500

    Completed InsertNodeAtTail

--- problem.js
+++ problem.js
@@ -1,13 +1,4 @@
-
-/*
- * For your reference:
- *
- * SinglyLinkedListNode {
- *     int data;
- *     SinglyLinkedListNode next;
- * }
- *
- */
+// Insert a Node at the Head of a Linked List
 function insertNodeAtHead(head, data) {
     let  current =head;
     // head can be Null too
@@ -19,4 +10,23 @@ function insertNodeAtHead(head, data) {
 }
 
 const res = insertNodeAtHead({data:320,next:{data:321,next:null}},324);
-console.log(res);
+// console.log(res);
+
+
+// Insert a Node at the Tail of a Linked List
+function insertNodeAtTail(head, data) {
+    let  current =head;
+    // head can be Null too
+    if(current === null){
+    return head = { data:data, next:null };
+    }
+    // If Head is not Nulll
+  while(current.next !== null ){
+    current = current.next;
+  }
+  current.next ={data:data, next:null};
+  return head;
+}
+
+const res2 = insertNodeAtTail({data:320,next:null},324);
+console.log(res2);
