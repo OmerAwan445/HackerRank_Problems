baa592b00466fcb3e0a02e4d59c78d371628c88d
Muhammad Umer <omerawan445@gmail.com>
2023-05-30 03:34:27 +0500

    Completed

--- problem.js
+++ problem.js
@@ -1,22 +1,31 @@
-// =============
-function hourglassSum(arr) {
-    let currSum = 0;
-    let maxSum;
-    //loop running rows
-    for (let i = 0; i < arr.length - 2; i++) {
-    //loop running columns
-        for (let j = 0; j < arr[i].length - 2; j++) {
-            currSum=arr[i][j]+arr[i][j+1]+arr[i][j+2];
-            currSum+=arr[i+1][j+1];
-            currSum+=arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
-            if(i==0 && j==0) maxSum = currSum;
-        if(maxSum < currSum) maxSum = currSum;
-        currSum = 0;
+function deleteNode(llist, position) {
+    // Write your code here
+    let current = llist;
+    let counter =0;
+    // Previous is null att start becasue at start no previous node
+    let previous=current;
+    // If Starting node
+    if(position === 0 && current.next !== null) return llist = {...current.next};
+    while(position !== counter){
+        previous = current
+        current=current.next;
+        counter++;
     }
-}
-    return maxSum;
-}
 
-const twoDArr = [[-1, -1, 0, -9, -2, -2], [-2, -1, -6, -8, -2, -5], [-1, -1, -1, -2, -3, -4], [-1, -9, -2 ,-4 ,-4 ,-5], [-7, -3, -3 ,-2, -9, -9], [-1, -3, -1, -2, -4 ,-5]];
-const res = hourglassSum(twoDArr);
-console.log(res)
+    if(current.next === null ){
+        // If one node
+        if(previous.next === null)
+        {
+            return llist = null;
+        }
+        // If Last node
+        previous.next =null;
+        return llist;
+    }
+    // If any other node
+    previous.next = {...current.next};
+    return llist;
+    }
+const res =deleteNode({data:1,next:null},0)
+console.log(res);
+
