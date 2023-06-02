a219f85175df9d3537c7e41ca0c5383c093241fe
Muhammad Umer <omerawan445@gmail.com>
2023-06-03 02:59:12 +0500

    completed

--- problem.js
+++ problem.js
@@ -1,22 +1,40 @@
-// =============
-function hourglassSum(arr) {
-    let currSum = 0;
-    let maxSum;
-    //loop running rows
-    for (let i = 0; i < arr.length - 2; i++) {
-    //loop running columns
-        for (let j = 0; j < arr[i].length - 2; j++) {
-            currSum=arr[i][j]+arr[i][j+1]+arr[i][j+2];
-            currSum+=arr[i+1][j+1];
-            currSum+=arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
-            if(i==0 && j==0) maxSum = currSum;
-        if(maxSum < currSum) maxSum = currSum;
-        currSum = 0;
-    }
+// Complete the CompareLists function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+
+function CompareLists(llist1, llist2) {
+   let firstList= llist1;
+   let secondList= llist2;
+    while(firstList !==null || secondList!==null ){
+    if(firstList=== null || secondList === null) return 0
+    if(firstList.data !== secondList.data) return 0
+    firstList= firstList.next;
+    secondList =secondList.next;
 }
-    return maxSum;
+return 1;
 }
 
-const twoDArr = [[-1, -1, 0, -9, -2, -2], [-2, -1, -6, -8, -2, -5], [-1, -1, -1, -2, -3, -4], [-1, -9, -2 ,-4 ,-4 ,-5], [-7, -3, -3 ,-2, -9, -9], [-1, -3, -1, -2, -4 ,-5]];
-const res = hourglassSum(twoDArr);
-console.log(res)
+const res= CompareLists(
+    {   date:2,
+        next:{
+            data:3,
+            next:null
+}},
+{   date:2,
+    next:{
+        data:3,
+        next:{
+            data:3,
+            next:null
+        }
+}}
+)
+console.log(res);
