7616257171daeb011b779883bf0359d896ced506
Muhammad Umer <omerawan445@gmail.com>
2023-06-08 11:12:49 +0500

    Completed

--- problem.js
+++ problem.js
@@ -1,22 +1,49 @@
-// =============
-function hourglassSum(arr) {
-    let currSum = 0;
-    let maxSum;
-    //loop running rows
-    for (let i = 0; i < arr.length - 2; i++) {
-    //loop running columns
-        for (let j = 0; j < arr[i].length - 2; j++) {
-            currSum=arr[i][j]+arr[i][j+1]+arr[i][j+2];
-            currSum+=arr[i+1][j+1];
-            currSum+=arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
-            if(i==0 && j==0) maxSum = currSum;
-        if(maxSum < currSum) maxSum = currSum;
-        currSum = 0;
+/*
+ * Complete the 'getNode' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_SINGLY_LINKED_LIST llist
+ *  2. INTEGER positionFromTail
+ */
+    function getNode(llist, positionFromTail) {
+        const data =[]
+        while(llist){
+        data.push(llist.data)
+        llist= llist.next;
+        }
+        return data[data.length-1 -positionFromTail]
     }
-}
-    return maxSum;
-}
+    const res = getNode({   data:2,
+    next:{
+        data:3,
+        next:{
+            data:4,
+            next:null
+        }
+}},2);
+console.log(res);
 
-const twoDArr = [[-1, -1, 0, -9, -2, -2], [-2, -1, -6, -8, -2, -5], [-1, -1, -1, -2, -3, -4], [-1, -9, -2 ,-4 ,-4 ,-5], [-7, -3, -3 ,-2, -9, -9], [-1, -3, -1, -2, -4 ,-5]];
-const res = hourglassSum(twoDArr);
-console.log(res)
+
+
+
+
+
+
+
+
+
+
+        /* // base Case
+        if(llist.next === null) return;
+        data = getNode(llist.next ,--positionFromTail);
+        // console.log(++count);
+        if(positionFromTail === 0 ){
+            console.log(positionFromTail, count);
+            console.log(llist.data);
+            return llist.data
+        }
+        else {
+            ++count;
+            return data
+        } */
